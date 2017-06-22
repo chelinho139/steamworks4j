9d8e0094f00ae5a0ef0c6444502207df8c920f1e
Alrik Geselle <lord_alrik@web.de>
2017-06-22 10:05:41 +0200

    Added custom build script

---
+++ build-natives/build.bat
@@ -0,0 +1,9 @@
+SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
+
+premake5 --file=build-win.lua vs2013
+msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild
+move bin\x32\ReleaseDLL\steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j.dll
+move bin\x32\ReleaseDLL\steamworks4j-server.dll ..\server\src\main\resources\steamworks4j-server.dll
+msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Rebuild
+move bin\x64\ReleaseDLL\steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j64.dll
+move bin\x64\ReleaseDLL\steamworks4j-server.dll ..\server\src\main\resources\steamworks4j-server64.dll
